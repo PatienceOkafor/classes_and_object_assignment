63ff90cd0aeca25236e7584200c1a731caea840b
PatienceOkafor <ukamakaokafor20@gmail.com>
2022-09-06 08:29:27 +0100

    Weekend_assignment_on_classes_and_object

---
+++ OOP(classses_and_Object).py
@@ -0,0 +1,117 @@
+class Cars:
+    def __init__(self,model,colors,year,manufacturer):
+        self.model = model
+        self.colors = colors
+        self.year = year
+        self.manufacturer = manufacturer
+
+Venza = Cars("XU80 series Harrier","Red",2020,"Toyota")
+Bmw = Cars("1 series(F40)","Black",2019,"BMW AG")
+Acura = Cars("mdx",1986"Honda")
+Lambogini = Cars("Urus(SUV)","white",2017,"luxury sport cars")
+Ferrari = Cars("F430 Spider", "red",2018,"Enzo ferrari")
+
+class Person:
+    def __init__(self,name,age,sex,height):
+        self.name = name
+        self.age = age
+        self.sex = sex
+        self.height = height
+
+Patience = Person("Patience",33,"Female",1.75)
+James = Person("James",21,"Male",1.55)
+Teejah = Person("Teejah",16,"Male",1.50)
+David = Person("David",20,"Male",1.65)
+Onwumi = Person("Onwumi",30,"Female",1.60)
+
+class Dog:
+    def __init__(self,name,color,character):
+        self.name = name
+        self.color = color
+        self.character = character
+
+Akita = Dog("Akita","black","easily trained")
+Boxer = Dog("Boxer","brown","Courageous")
+Bulldog = Dog("Bulldog","black","intelligent")
+Chow_Chow = Dog("Chow_Chow","white","Strong_willed")
+Pug = Dog("Pug","Maroun","affectionate")
+
+class Laptop:
+    def __init__(self,brand,color,year_manufacture):
+        self.brand = brand
+        self.color = color
+        self.year_manufacture = year_manufacture
+
+Hp = Laptop("RGB","Blue",1981)
+Lenovo = Laptop()
+Asus = Laptop()
+Dell = Laptop()
+Apple = Laptop()
+
+class Shark:
+    animal_type = " "
+    location = " "
+    followers = " "
+
+sammy = Shark()
+philip = Shark()
+stevie = Shark()
+mile = Shark()
+lary = Shark()
+
+class Parrot:
+    species = " "
+    name = " "
+    age = " "
+
+blu = Parrot()
+woo = Parrot()
+roo = Parrot()
+qua = Parrot()
+oot = Parrot()
+
+class Country:
+    name_country = " "
+    capital = " "
+    continent = " "
+
+Nigeria = Country()
+Cameroon = Country()
+India = Country()
+Sweden = Country()
+Britain = Country()
+
+class Employee:
+    emp_name = " "
+    salary = " "
+    Gender = " "
+
+Mary = Employee()
+john = Employee()
+Sandra = Employee()
+Paul = Employee()
+Peace = Employee()
+
+class Food:
+    def __init__(self,type,taste,color):
+        self.type = type
+        self.taste = taste
+        self.color = color
+
+Rice = Food()
+stew = Food()
+beans = Food()
+spaghetti = Food()
+charwama = Food()
+
+class Pet:
+    def __init__(self,choice,color,movement):
+        self.choice = choice 
+        self.color = color
+        self.movement = movement
+
+fish = Pet("Goldfish","orange","swim")
+reptiles = Pet("Tortoise","black","slow_moving")
+amphibians =  Pet("Tree Frog","fast_moving")
+birds = Pet("Parrot","green","fly")
+mammals = Pet("cat","white","walk")
